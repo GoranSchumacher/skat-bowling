23c249e5c3a885594b4550992971674be73bcf1f
Göran Schumacher <schumacher@me.com>
2016-10-14 10:19:28 +0200

    Extracted calculateStrike and calculateSpare as local methods.

--- src/main/scala/com/skat/bowling/com/skat/caseclasses/Frame.scala
+++ src/main/scala/com/skat/bowling/com/skat/caseclasses/Frame.scala
@@ -25,32 +25,43 @@ case class Frame(var throw1: Option[Int]=None,var throw2: Option[Int]=None,var  
    * @return The total
    */
   def pointsForFrame(running: Int): Int = {
+
+    def calculateStrike: Int = {
+      if (nextFrame.isDefined) {
+        nextFrame.get.throw1.getOrElse(0) + //First extra point
+          (if (nextFrame.get.throw2.isDefined && nextFrame.get.throw2.get != 0) {
+            nextFrame.get.throw2.get // Second extra point
+          } else {
+            if (nextFrame.get.nextFrame.isDefined && nextFrame.get.nextFrame.get.throw1.isDefined) {
+              nextFrame.get.nextFrame.get.throw1.get
+            } else {
+              0 // Error or ball not thrown yet
+            }
+          }
+            )
+      } else {
+        0 // Error or ball not thrown yet
+      }
+    }
+
+    def calculateSpare: Int = {
+      if (nextFrame.isDefined)
+        nextFrame.get.throw1.getOrElse(0) //First extra point
+      else
+        0 // Error or ball not thrown yet
+    }
+
     val runningPlusThisFrame=running+throw1.getOrElse(0)+throw2.getOrElse(0)+throw3.getOrElse(0)
+
     val nextFrameExtraPoints: Int = {
       if(throw1.getOrElse(0)==10) {                             // Strike
-        if(nextFrame.isDefined) {
-          nextFrame.get.throw1.getOrElse(0)+                    //First extra point
-            (if(nextFrame.get.throw2.isDefined && nextFrame.get.throw2.get!=0) {
-              nextFrame.get.throw2.get                          // Second extra point
-            } else {
-              if (nextFrame.get.nextFrame.isDefined && nextFrame.get.nextFrame.get.throw1.isDefined) {
-                nextFrame.get.nextFrame.get.throw1.get
-            } else {
-            0       // Error or ball not thrown yet
-          }
-            }
-              )
-        } else {
-          0       // Error or ball not thrown yet
-        }
+        calculateStrike
     } else if((throw1.getOrElse(0)+throw2.getOrElse(0))==10) {  // Spare
-      if(nextFrame.isDefined)
-        nextFrame.get.throw1.getOrElse(0)                       //First extra point
-      else
-        0         // Error or ball not thrown yet
+      calculateSpare
     } else
       0           // Error or ball not thrown yet
     }
     runningPlusThisFrame+nextFrameExtraPoints
+
   }
 }
